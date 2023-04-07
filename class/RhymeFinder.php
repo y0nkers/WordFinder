221ccf3b83dc2ec35313182c3cc5cdcc2fe50f06
y0nkers <y0nkerswastaken@gmail.com>
2023-04-07 18:09:44 +0300

    Поиск рифм вынесен в класс RhymeFinder

---
+++ class/RhymeFinder.php
@@ -0,0 +1,83 @@
+<?php
+
+// Класс для нахождения слов-рифм к указанному слову
+class RhymeFinder
+{
+    private DbConnect $_connect; // Подключение к БД
+    private string $_word; // Слово, к которому нужно найти рифму
+    private string $_end; // Окончание слова
+
+    function __construct(DbConnect $connect, string $word)
+    {
+        $this->_connect = $connect;
+        $this->_word = $word;
+    }
+
+    // Основной метод поиска
+    public function find(): array
+    {
+        $length = strlen($this->_word);
+        if ($length < 3) {
+            return [
+                "status" => false,
+                "message" => "Не удалось найти рифму к слову " . $this->_word
+            ];
+        }
+        if ($length == 3) $this->_end = mb_substr($this->_word, -2, null,'UTF-8');
+        else $this->_end = mb_substr($this->_word, -3, null,'UTF-8');
+
+        $dictionaries = $this->getDictionaries();
+        $query = $this->constructQuery($dictionaries);
+        $results = $this->executeQuery($query);
+        $html = $this->constructHTML($results);
+        return [
+            "status" => true,
+            "message" => $html
+        ];
+    }
+
+    // Получение всех доступных словарей в системе
+    private function getDictionaries(): array
+    {
+        $dictionaries = array();
+        $stmt = $this->_connect->getPDO()->query("SELECT `id` FROM `dictionaries`");
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) $dictionaries[] = $row["id"];
+        return $dictionaries;
+    }
+
+    // Подготовка строки запроса
+    private function constructQuery(array $dictionaries): string
+    {
+        $query = "";
+        $count = count($dictionaries);
+        foreach ($dictionaries as $index => $dictionary) {
+            $query .= "SELECT `word` FROM " . "dictionary_" . $dictionary . " WHERE word LIKE '%$this->_end' AND word != '$this->_word'";
+            if ($index != $count - 1) $query .= " UNION ";
+        }
+        return $query;
+    }
+
+    // Выполнение запроса и получение его результата
+    private function executeQuery(string $query): array
+    {
+        $stmt = $this->_connect->getPDO()->query($query);
+        $results = array();
+        $count = $stmt->rowCount();
+        if ($count > 0) while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) $results[] = $row["word"];
+        return $results;
+    }
+
+    // Создание html элемента с результатами запроса
+    private function constructHTML(array $results): string
+    {
+        $html = "<div class='container mt-3'><h2 class='text-center'>Результаты поиска:</h2>";
+        if (count($results) > 0) {
+            $html .= "<div style='line-height: 1.4em;'>";
+            for ($i = 0; $i < count($results); $i++) $html .= $results[$i]["word"] . " ";
+        } else {
+            $html .= "<p>Не найдены подходящие результаты для указанного запроса.</p>";
+        }
+        $html .= "</div>";
+        return $html;
+    }
+}
--- core/rhyme.php
+++ core/rhyme.php
@@ -2,60 +2,13 @@
 if ($_SERVER["REQUEST_METHOD"] != "GET") die();
 
 require_once "../class/DbConnect.php";
-$dbConnect = new DbConnect("user", "");
-$pdo = $dbConnect->getPDO();
+require_once "../class/RhymeFinder.php";
 
 $word = $_GET["word"];
-$length = strlen($word);
-if ($length < 3) {
-    $response = [
-        "status" => false,
-        "message" => "Не удалось найти рифму к слову " . $word
-    ];
-    echo json_encode($response);
-    die();
-}
+$dbConnect = new DbConnect("user", "");
+$finder = new RhymeFinder($dbConnect, $word);
 
-if ($length == 3) $end = mb_substr($word, -2, null,'UTF-8');
-else $end = mb_substr($word, -3, null,'UTF-8');
-
-// 1. Найти все словари
-$dictionaries = array();
-$stmt = $pdo->query("SELECT `id` FROM `dictionaries`");
-while ($row = $stmt->fetch()) $dictionaries[] = $row["id"];
-
-// 2. Сформировать строку запроса по всем словарям
-$query = "";
-$count = count($dictionaries);
-foreach ($dictionaries as $index => $dictionary) {
-    $query .= "SELECT * FROM " . "dictionary_" . $dictionary . " WHERE word LIKE '%$end' AND word != '$word'";
-    if ($index != $count - 1) $query .= " UNION ";
-}
-
-// 3. Выполнить запрос, получить данные
-$stmt = $pdo->query($query);
-$results = [];
-$count = $stmt->rowCount();
-if ($count > 0)
-    while ($row = $stmt->fetch()) $results[] = $row;
-
-// 4. Вернуть данные
-$html_string = "<div class='container mt-3'>";
-$html_string .= "<h2 class='text-center'>Результаты поиска:</h2>";
-if ($count > 0) {
-    $html_string .= "<div style='line-height: 1.4em;'>";
-    for ($i = 0; $i < count($results); $i++) $html_string .= $results[$i]["word"] . " ";
-    $html_string .= "</div>";
-} else {
-    $html_string .= "<p>Не найдены подходящие результаты для указанного запроса.</p>";
-    $html_string .= "</div>";
-}
-
-$response = [
-    "status" => true,
-    "message" => $html_string,
-    "query" => $query
-];
+$response = $finder->find();
 echo json_encode($response);
 
 $dbConnect->closeConnection();
