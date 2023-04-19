4cc7d67ddfa54f520d8be04fce3b2bfebe100229
y0nkers <y0nkerswastaken@gmail.com>
2023-04-19 17:35:07 +0300

    login.php fix

--- admin/login.php
+++ admin/login.php
@@ -26,8 +26,7 @@ if (isset($_GET['login']) && isset($_GET['password'])) {
         <title>Вход в админ панель</title>
         <link rel="icon" type="image/png" href="/assets/img/favicon.ico">
         <link rel="stylesheet" href="/assets/css/style.css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+        <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
     </head>
     <body>
 
@@ -55,7 +54,7 @@ if (isset($_GET['login']) && isset($_GET['password'])) {
         </div>
 
         <!-- Bootstrap and jQuery scripts -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-        <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+        <script src="/assets/js/bootstrap.bundle.min.js"></script>
+        <script src="/assets/js/jquery-3.6.3.min.js"></script>
     </body>
 </html>
